6d9466e8e71f6710b9ddcf3e0bfbaef10f1317cb
Daniel Fairhurst <daniel.fairhurst.82@gmail.com>
2019-11-15 12:19:14 -0500

    Ensure avatar is a jpeg, and if any errors do not block login

--- pokerscores/pokerscores/social.py
+++ pokerscores/pokerscores/social.py
@@ -2,20 +2,23 @@ import requests
 
 def get_avatar(backend, strategy, details, response,
         user=None, *args, **kwargs):
-    url = None
-    if backend.name == 'facebook':
-        url = "http://graph.facebook.com/%s/picture?type=large"%response['id']
-    if backend.name == 'twitter':
-        url = response.get('profile_image_url', '').replace('_normal','')
-    if backend.name == 'google-oauth2':
-        print(response)
-        url = response.get('picture', None)
-        ext = url.split('.')[-1]
-    if url:
-        r = requests.get(url)
-        if r.status_code == 200:
-            print(r.headers['content-length'])
-            user.profile.avatar = r.content
-            print(len(user.profile.avatar))
-            #user.save()
-            user.profile.save()
+    try:
+        url = None
+        if backend.name == 'facebook':
+            url = "http://graph.facebook.com/%s/picture?type=large"%response['id']
+        if backend.name == 'twitter':
+            url = response.get('profile_image_url', '').replace('_normal','')
+        if backend.name == 'google-oauth2':
+            print(response)
+            url = response.get('picture', None)
+            ext = url.split('.')[-1]
+        if url:
+            print(url)
+            r = requests.get(url)
+            if r.status_code == 200:
+                print(r.headers)
+                if r.headers.get('content-type') in ('image/jpg', 'image/jpeg'):
+                    user.profile.avatar = r.content
+                    user.profile.save()
+    except:
+        pass
