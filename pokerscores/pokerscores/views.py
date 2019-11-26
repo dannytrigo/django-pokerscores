b67d9457082ff55932df4f24acb074daa57f3a91
Daniel Fairhurst <daniel.fairhurst.82@gmail.com>
2019-11-25 19:01:29 -0500

    Retrieve index file with the staticfiles module instead of hardcoded location

--- pokerscores/pokerscores/views.py
+++ pokerscores/pokerscores/views.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
-from os.path import dirname, basename, join as mkpath
 from django.contrib.staticfiles.finders import find
 
+
 def angular_home(request):
-    index_file = find('index.html')
-    print(index_file)
-    with open(mkpath(dirname(dirname(dirname(__file__))), 'pokerui', 'build', 'ang', 'index.html'), 'rb') as index_file:
-        data = index_file.read()
-        return HttpResponse(data)
+    index_file = find('ang/index.html')
+    if index_file:
+        with open(index_file, 'rb') as index_file:
+            data = index_file.read()
+            return HttpResponse(data)
     return HttpResponse(status=500)
